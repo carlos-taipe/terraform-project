cb31e402e7cdb4d0d84fc00251f1de7c3797b2d6
Carlos Alberto Taipe Casas <hakkoben@Carloss-MacBook-Air.local>
2026-05-05 20:47:41 -0500

    Fixing ingress from internet for ec2

--- main.tf
+++ main.tf
@@ -8,12 +8,13 @@ resource "aws_instance" "nginx_server" {
     ami           = "ami-0ed094fb1304fd857" # Amazon Linux 2 AMI
     instance_type = "t3.micro"
 
+    associate_public_ip_address = true 
+
     user_data = <<-EOF
                 #!/bin/bash
-                sudo yum update -y
-                sudo amazon-linux-extras install nginx1.12 -y
-                sudo systemctl start nginx
+                sudo yum install -y nginx
                 sudo systemctl enable nginx
+                sudo systemctl start nginx
             EOF
     
     key_name = aws_key_pair.nginx-server-ssh.key_name
@@ -53,4 +54,8 @@ resource "aws_security_group" "nginx-server-sg" {
 resource "aws_key_pair" "nginx-server-ssh" {
     key_name   = "nginx-server-ssh"
     public_key = file("nginx-server.key.pub")
+}
+
+output "public_ip" {
+  value = aws_instance.nginx_server.public_ip
 }
